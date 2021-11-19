973f9c86e594d1c92ee46b40c8f30bc3fa1ee135
paumonedero <pmonederor2021@cepnet.net>
2021-11-19 10:36:49 +0100

    Updated

--- webAppEnBlanc/js/events.js
+++ webAppEnBlanc/js/events.js
@@ -1,1 +1,12 @@
+function llenguatgeNavegador(){
+    let llenguatge = navigator.language;
+    document.getElementById("llenguatge").innerHTML = "Llenguatge: " + llenguatge;
+}
 
+function canviaText(id){
+    id.innerHTML = Date();
+}
+
+function majuscules(){
+    
+}
